56bebe26e1665b75323e1fc56eadbd9148e04e7b
Sérgio Ribeiro <nb25172@novabase.pt>
2024-10-31 12:45:58 +0000

    basic list only

---
+++ app/src/main/java/com/example/batata/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.batata;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.main_activity);
+
+
+        ListView listView = findViewById(R.id.myListViewer);
+
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("html");
+        strings.add("java");
+        strings.add("kotlin");
+        strings.add("python");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,strings);
+
+        listView.setAdapter(arrayAdapter);
+
+       listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object listItem = listView.getItemAtPosition(position);
+            }
+        });
+
+
+    }
+}
